60d7ff1980199225e8eff878c9f53209c87361c6
Bhargav Karanam <bhargav.karanam@gmail.com>
2015-10-20 22:03:23 +0530

    Added comments

--- login.php
+++ login.php
@@ -2,10 +2,12 @@
 require_once("dbconnection.php");
 $username=mysqli_real_escape_string($con,$_POST['username']);
 $password=mysqli_real_escape_string($con,$_POST['password']);
+//hashing the password using  md5
 $password=md5($password);
 $sql="select * from member_details where Username='$username' and Password='$password'";
 $result=mysqli_query($con,$sql);
 $count=mysqli_num_rows($result);
+//check readme.md for status codes
 if($count==1)
 {
 	$send=array(
