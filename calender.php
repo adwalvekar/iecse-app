ac5717fc4d3d0dbb6730856dd5af8b633bf454d0
adityaswalvekar <addi61195@gmail.com>
2015-11-04 17:36:38 +0530

    First push

---
+++ calender.php
@@ -0,0 +1,5 @@
+<?php
+require_once(__DIR__.'/dbconnection.php');
+$db = new dbcon();
+$dbaccess= new dbaccess();
+
--- dbconnection.php
+++ dbconnection.php
@@ -1,3 +1,17 @@
 <?php
-$con=mysqli_connect("localhost","root","","iecse_app");
-?>
+class dbcon{
+
+	public function __construct(){
+
+		$json=json_decode(__DIR__.'/config.json');
+		$username = $json['username'];
+		$server = $json['server'];
+		$password = $json['password'];
+		$database = $json['database'];
+		$conn= new mysqli($server,$username,$password,$database);
+		if (!$conn){
+			echo json_encode("status : 504");
+		}
+	}
+
+}
