fdeb9c39483c3e34d3eec95812e1a7ff676bdefe
Aditya Walvekar <aditya@Adityas-MacBook-Pro.local>
2016-01-16 23:25:50 +0530

    Actually fixed the JSON

--- iecse-app-backend/calendar.php
+++ iecse-app-backend/calendar.php
@@ -1,6 +1,7 @@
 <?php 
 include_once('dbaccess.php');
 
+
 class calendar{
 	public $conn, $dbaccess;
 	public function __construct(){
@@ -14,15 +15,23 @@ class calendar{
 		$q="SELECT * FROM events WHERE month= $month";
 		$r1=mysqli_query($conn,$q);
 		if(mysqli_num_rows($r1)>=1){
-		$a=array();
-		$a[0]=array('status'=>'611');
-		$i=1;
+		echo '{';
+		echo '"status":"611",';
+		echo '"events":';
 		echo '[';
+		$num=mysqli_num_rows($r1);
+		$i=0;
 		while($q1_arr=mysqli_fetch_assoc($r1)){
 			$a=array('name'=>$q1_arr['name'],'date'=>$q1_arr['date'],'month'=>$q1_arr['month'],'year'=>$q1_arr['year'],'location'=>$q1_arr['location'],'description'=>$q1_arr['description'],'imageURL'=>$q1_arr['imgURL']);
 			echo json_encode($a);
+			if($num-$i!=1){
+				echo ',';
+
+			}
+			$i++;
 		}
 		echo ']';
+		echo'}';
 		
 	}
 	else echo json_encode(array('status'=>'604'));
